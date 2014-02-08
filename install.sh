3773dc9ce6ba4ad29ccddb94c1f534c1287e6a27
Victor Passapera <vpassapera@gmail.com>
2014-02-08 04:12:29 -0800

    Adding bash files

---
+++ install.sh
@@ -0,0 +1,7 @@
+rm ../.bash_aliases ../.bash_exports ../.bash_logout ../.bash_profile ../.bashrc ../.bash_symfony_autocomplete
+ln -s bash_aliases ../.bash_aliases
+ln -s bash_exports ../.bash_exports
+ln -s bash_logout ../.bash_logout
+ln -s bash_profile ../.bash_profile
+ln -s bashrc ../.bashrc
+ln -s bash_symfony_autocomplete ../.bash_symfony_autocomplete
