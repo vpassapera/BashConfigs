8a799ea924e7fde6b09a96163d173756c1c7031c
Victor Passapera <vpassapera@gmail.com>
2014-02-08 06:16:08 -0800

    Fixing all

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 rm .bash_aliases .bash_exports .bash_logout .bash_profile .bashrc .bash_symfony_autocomplete 2> /dev/null
-ln -s bash_aliases ../.bash_aliases
-ln -s bash_exports ../.bash_exports
-ln -s bash_logout ../.bash_logout
-ln -s bash_profile ../.bash_profile
-ln -s bashrc ../.bashrc
-ln -s bash_symfony_autocomplete ../.bash_symfony_autocomplete
+ln -s .bash/bash_aliases .bash_aliases
+ln -s .bash/bash_exports .bash_exports
+ln -s .bash/bash_logout .bash_logout
+ln -s .bash/bash_profile .bash_profile
+ln -s .bash/bashrc .bashrc
+ln -s .bash/bash_symfony_autocomplete .bash_symfony_autocomplete
