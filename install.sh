918a7011a31701212e009bbfc36d1aea61352372
Victor Passapera <vpassapera@gmail.com>
2014-02-08 21:54:33 -0800

    Fixing output

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
-echo "Removing previous bash configurations"...
-rm $HOME/.bash_aliases $HOME/.bash_exports $HOME/.bash_logout $HOME/.bash_profile $HOME/.bashrc $HOME/.bash_symfony_autocomplete 2> /dev/null
-
 echo "Enabling Aliases..."
-rm -r $HOME/.bash_it/aliases/enabled
+rm -r $HOME/.bash_it/aliases/enabled 2> /dev/null
 mkdir -p $HOME/.bash_it/aliases/enabled
 ln -s $HOME/.bash_it/aliases/available/general.aliases.bash $HOME/.bash_it/aliases/enabled/general.aliases.bash
 ln -s $HOME/.bash_it/aliases/available/git.aliases.bash $HOME/.bash_it/aliases/enabled/git.aliases.bash
 ln -s $HOME/.bash_it/aliases/available/vagrant.aliases.bash $HOME/.bash_it/aliases/enabled/vagrant.aliases.bash
 
 echo "Enabling Completion..."
-rm -r $HOME/.bash_it/completion/enabled
+rm -r $HOME/.bash_it/completion/enabled 2> /dev/null
 mkdir -p $HOME/.bash_it/completion/enabled
 ln -s $HOME/.bash_it/aliases/available/bash-it.completion.bash $HOME/.bash_it/aliases/enabled/bash-it.completion.bash
 ln -s $HOME/.bash_it/aliases/available/capistrano.completion.bash $HOME/.bash_it/aliases/enabled/capistrano.completion.bash
@@ -21,13 +18,16 @@ ln -s $HOME/.bash_it/aliases/available/grunt.completion.bash $HOME/.bash_it/alia
 ln -s $HOME/.bash_it/aliases/available/ssh.completion.bash $HOME/.bash_it/aliases/enabled/ssh.completion.bash
 
 echo "Enabling Plugins..."
-rm -r $HOME/.bash_it/plugins/enabled
+rm -r $HOME/.bash_it/plugins/enabled 2> /dev/null
 mkdir -p $HOME/.bash_it/plugins/enabled
 ln -s $HOME/.bash_it/plugins/available/base.plugin.bash $HOME/.bash_it/plugins/enabled/base.plugin.bash
 ln -s $HOME/.bash_it/plugins/available/extract.plugin.bash $HOME/.bash_it/plugins/enabled/extract.plugin.bash
 ln -s $HOME/.bash_it/plugins/available/git.plugin.bash $HOME/.bash_it/plugins/enabled/git.plugin.bash
 ln -s $HOME/.bash_it/plugins/available/ssh.plugin.bash $HOME/.bash_it/plugins/enabled/ssh.plugin.bash
 ln -s $HOME/.bash_it/plugins/available/vagrant.plugin.bash $HOME/.bash_it/plugins/enabled/vagrant.plugin.bash
+
+echo "Removing previous bash configurations"...
+rm $HOME/.bash_aliases $HOME/.bash_exports $HOME/.bash_logout $HOME/.bash_profile $HOME/.bashrc $HOME/.bash_symfony_autocomplete 2> /dev/null
 
 echo "Creating new links..."
 ln -s $HOME/.bash/bash_aliases $HOME/.bash_aliases
